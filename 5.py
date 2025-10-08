9d0ef2926dde8ee9dbb0270ad473bd48c7af4b8d
Poojitha0307 <192421257.simats@saveetha.com>
2025-10-08 09:16:57 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,10 @@
+list1 = [1, 2, 3, ['a', 'b', 'c']]
+
+print("Nested List:", list1[3])
+print("Length:", len(list1))
+print("Concatenation:", list1 + [4, 5])
+print("Membership:", 2 in list1)
+print("Iteration:")
+for item in list1: print(item)
+print("Indexing:", list1[0])
+print("Slicing:", list1[1:3])
