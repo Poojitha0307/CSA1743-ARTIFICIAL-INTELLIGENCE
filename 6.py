c4f02ab3cdd31954311016a107914e7337469a48
Poojitha0307 <192421257.simats@saveetha.com>
2025-10-08 09:17:21 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,9 @@
+lst = [1, 2, 3]
+lst.append(4)
+print("After Append:", lst)
+lst.extend([5, 6])
+print("After Extend:", lst)
+lst.insert(1, 10)
+print("After Insert:", lst)
+del lst[2]
+print("After Delete:", lst)
